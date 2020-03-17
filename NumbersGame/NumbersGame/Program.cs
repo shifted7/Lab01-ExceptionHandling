9a28e23d2f41f4cfbe2d6046b6f5a62c50f6ad9f
Andrew <shifted6@gmail.com>
2020-03-17 11:04:59 -0700

    Lab created, populates array based on user input, handling exceptions.

---
+++ NumbersGame/NumbersGame/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace NumbersGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the number game!");
+            StartSequence();
+        }
+        static void StartSequence()
+        {
+            try
+            {
+                Console.WriteLine($"Please enter a number greater than zero: ");
+                string userInput = Console.ReadLine();
+                int[] userNumbers = new int[Convert.ToInt32(userInput)];
+                Populate(userNumbers);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        static int[] Populate(int[] array)
+        {
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Please enter a number ({i}/{array.Length} entered): ");
+                string userInput = Console.ReadLine();
+                array[i] = Convert.ToInt32(userInput);
+            }
+            return array;
+        }
+    }
+}
